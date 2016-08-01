167fdeeac31537ddc64f6bf4e2e7f0bcd9631b14
Christof Stocker <c.stocker@hci-kdd.org>
2016-08-01 09:37:04 +0200

    fix deprecated type conversion

--- src/polyline.jl
+++ src/polyline.jl
@@ -5,10 +5,10 @@ function inpolygon{T<:Number}(x:: T, y:: T, vx:: Vector{T}, vy:: Vector{T})
     c = false
     j = length(vx)
     @inbounds for i=1:length(vx)
-        if (((vy[i] <= y && y < vy[j]) || 
-            (vy[j] <= y && y < vy[i])) && 
+        if (((vy[i] <= y && y < vy[j]) ||
+            (vy[j] <= y && y < vy[i])) &&
             (x < (vx[j] - vx[i]) * (y - vy[i]) / (vy[j] - vy[i]) + vx[i]))
-            c = !c 
+            c = !c
         end
         j = i
     end
@@ -17,22 +17,22 @@ end
 
 
 function fillpoly!{T,P<:Number}(M::Matrix{T}, px::Vector{P}, py::Vector{P}, value::T)
-    @assert length(px) == length(py)    
-    left, right = int(minimum(px)), int(maximum(px))
-    top, bottom = int(minimum(py)), int(maximum(py))
-    for x=left:right     
+    @assert length(px) == length(py)
+    left, right = round(Int, minimum(px)), round(Int, maximum(px))
+    top, bottom = round(Int, minimum(py)), round(Int, maximum(py))
+    for x=left:right
         ys = Set{Int64}()
         j = length(px)
-        for i=1:length(px)            
-            if (px[i] <= x && x <= px[j]) || (px[j] <= x && x <= px[i])
-                # special case: adding the whole cut to ys                            
-                if px[i] == px[j]
-                    push!(ys, py[i])
-                    push!(ys, py[j])
+        for i=1:length(px)
+            if (px[i] <= x <= px[j]) || (px[j] <= x <= px[i])
+                # special case: adding the whole cut to ys
+                if abs(px[i] - px[j]) <= eps(P)
+                    push!(ys, round(Int, py[i]))
+                    push!(ys, round(Int, py[j]))
                 else
                     y = py[i] + (x - px[i]) / (px[j] - px[i]) * (py[j] - py[i])
-                    push!(ys, int(y))
-                end            
+                    push!(ys, round(Int, y))
+                end
             end
             j = i
         end
@@ -41,7 +41,7 @@ function fillpoly!{T,P<:Number}(M::Matrix{T}, px::Vector{P}, py::Vector{P}, valu
         if length(ys) % 2 == 1
             push!(ys, ys[end])
         end
-        for i=1:2:length(ys)           
+        for i=1:2:length(ys)
             M[ys[i]:ys[i+1], x] = value  # <-- bounds error here!
         end
     end
